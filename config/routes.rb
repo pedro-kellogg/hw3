9776e65db8f6c63410bc62d698790f05513fcb09
pedro-kellogg <pedro.albuquerque@kellogg.northwestern.edu>
2026-05-18 00:49:06 +0000

    routes placed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get("/", { :controller => "places", :action => "index" })
+
+  # Placing routes
+  get("/places",        { :controller => "places", :action => "index"  })
+  get("/places/new",    { :controller => "places", :action => "new"    })
+  post("/places",       { :controller => "places", :action => "create" })
+  get("/places/:id",    { :controller => "places", :action => "show"   })
+
+  # Entries
+  get("/places/:place_id/entries/new",  { :controller => "entries", :action => "new"    })
+  post("/places/:place_id/entries",     { :controller => "entries", :action => "create" })
+
 end
