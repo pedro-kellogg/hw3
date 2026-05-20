f5e0160bd7f9c44a02ae5588b5f87cceb46f71f9
pedro-kellogg <pedro.albuquerque@kellogg.northwestern.edu>
2026-05-20 05:04:44 +0000

    saving new place

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,22 @@
+class EntriesController < ApplicationController
+
+  def new
+    @entry = Entry.new
+    @place = Place.find(params[:place_id])
+  end
+
+  def create
+    @entry = Entry.new
+    @entry.title = params[:title]
+    @entry.description = params[:description]
+    @entry.occurred_on = params[:occurred_on]
+    @entry.place_id = params[:place_id]
+
+    if @entry.save
+      redirect_to "/places/#{@entry.place_id}"
+    else
+      redirect_to "/places/#{params[:place_id]}/entries/new"
+    end
+  end
+
+end
